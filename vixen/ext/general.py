264d8fb1b58d086a20e98df1942b2f9f6f0f1923
devnote-dev <pokeraven4@gmail.com>
2021-12-07 22:50:24 +0000

    feat(general): status command

--- vixen/ext/general.py
+++ vixen/ext/general.py
@@ -1,4 +1,6 @@
+from discord.embeds import Embed
 from discord.ext.commands import Cog, Bot, command
+import os
 
 
 class General(Cog):
@@ -13,6 +15,18 @@ class General(Cog):
             round(self.bot.latency * 1000),
             (msg.created_at.microsecond - ctx.message.created_at.microsecond) / 1000
         ))
+    
+    @command(description='Sends Vixen\'s status')
+    async def status(self, ctx):
+        embed = Embed(
+            title='Vixen Status',
+            color=0xe1c4b9
+        ).set_author(name='ᓚᘏᗢ', icon_url=self.bot.user.display_avatar.url)
+        embed.add_field(name='Uptime', value='<t:%d> (<t:%d:R>)' % (self.bot.uptime, self.bot.uptime), inline=False)
+        embed.add_field(name='Servers', value=str(len(self.bot.guilds)))
+        embed.add_field(name='Channels', value=len(self.bot.private_channels))
+        embed.add_field(name='Process', value='%iMB' % os.cpu_count())
+        return await ctx.send(embed=embed)
     
     @command(description='Smart math calculating stuff', aliases=['ma', 'm'])
     async def math(self, ctx, *, args: str):
